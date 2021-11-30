73af6262a5922b6265b9d3f0ba97fc78b88957fa
Shahid0120 <shahid.hussain0120@gmail.com>
2021-11-30 22:19:49 +1100

    had pre-rendering instructions by parsing md files for static static site generations and server-side rendering

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "23659b66548557d8"; }
+/******/ 		__webpack_require__.h = function() { return "b6b449ee908551bd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
